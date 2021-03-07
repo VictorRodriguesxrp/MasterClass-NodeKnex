b56ce8f1d1d41f5a222ac791cb2c2fa643dc9644
Victor Rodrigues <victorxrp@gmail.com>
2021-03-07 15:00:01 -0300

    Procedures and Triggers

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Youtube Masterclass NextJS
+
+## Construindo uma API de cadastro de Usuários e Projetos abordando os conceitos de Knex.js.
+## Foi abordado o uso de Migrations, Seeds, Error Handling, Soft Delete, Paginação, Procedures e Triggers.
--- src/database/migrations/20210307161655_create_projects_table.js
+++ src/database/migrations/20210307161655_create_projects_table.js
@@ -1,5 +1,6 @@
+const { onUpdateTrigger } = require('../../../knexfile')
 
-exports.up = (knex) => {
+exports.up = async (knex) => {
   return knex.schema.createTable('projects', (table) => {
     table.increments('id');
     table.text('title').notNullable();
@@ -12,10 +13,10 @@ exports.up = (knex) => {
 
     // table.timestamp('created_at').defaultTo(knex.fn.now());
     // table.timestamp('updated_at').defaultTo(knex.fn.now());
-    table.timestamps(true, true);
-  }) 
+    table.timestamps(true, true)
+  }).then(() => knex.raw(onUpdateTrigger('projects'))) 
 };
 
-exports.down = (knex) => {
+exports.down = async (knex) => {
   return knex.schema.dropTable('projects');
 };
